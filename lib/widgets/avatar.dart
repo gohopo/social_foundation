607ad6ea3f69adf49ddedfe8fa85f22c212e7542
gohopo <gohopo@live.com>
2020-12-09 15:01:33 +0800

    头像边框

--- lib/widgets/avatar.dart
+++ lib/widgets/avatar.dart
@@ -10,6 +10,7 @@ class SfAvatar extends StatelessWidget{
     this.width = 45,
     this.height = 45,
     this.onTap,
+    this.border,
     this.borderRadius,
     this.defaultImage,
     this.fit = BoxFit.cover,
@@ -20,37 +21,57 @@ class SfAvatar extends StatelessWidget{
   final double width;
   final double height;
   final VoidCallback onTap;
+  final BoxBorder border;
   final BorderRadiusGeometry borderRadius;
   final ImageProvider defaultImage;
   final BoxFit fit;
   final Widget Function(BuildContext context,SfAvatar avatar,ImageProvider image) builder;
   final int imageLongSide;
 
+  ImageProvider get imageProvider{
+    return user?.icon!=null ? SfCacheManager.provder(SfAliyunOss.getImageUrl(user?.icon,long:imageLongSide)) : defaultImage;
+  }
   onTapOverride(){
     onTap?.call();
   }
+  Widget buildDecoration(BuildContext context,Widget child){
+    return border!=null ? Container(
+      decoration: BoxDecoration(
+        border: border,
+        borderRadius: borderRadius??BorderRadius.circular((width??height??0)/2),
+      ),
+      child: child
+    ) : child;
+  }
+  Widget buildClip(BuildContext context,Widget child){
+    return borderRadius!=null ? ClipRRect(
+      borderRadius: borderRadius,
+      child: child
+    ) : ClipOval(
+      child: child,
+    );
+  }
   Widget buildImage(BuildContext context){
-    return builder!=null ? builder(context,this,buildImageProvider()) : Image(
+    return builder!=null ? builder(context,this,imageProvider) : Image(
       width: width,
       height: height,
-      image: buildImageProvider(),
+      image: imageProvider,
       fit: this.fit
     );
   }
-  ImageProvider buildImageProvider(){
-    return user?.icon!=null ? SfCacheManager.provder(SfAliyunOss.getImageUrl(user?.icon,long:imageLongSide)) : defaultImage;
-  }
+  
 
   @override
   Widget build(BuildContext context){
     return GestureDetector(
       onTap: onTapOverride,
-      child: borderRadius==null ? ClipOval(
-        child: buildImage(context),
-      ) : ClipRRect(
-        borderRadius: borderRadius,
-        child: buildImage(context)
-      ),
+      child: buildDecoration(
+        context,
+        buildClip(
+          context,
+          buildImage(context)
+        )
+      )
     );
   }
 }
