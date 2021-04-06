23e110af523a96fbc1b8dad8b5eb41be29e2d7a4
gohopo <gohopo@live.com>
2021-04-06 11:12:35 +0800

    svga升级

--- lib/utils/file_helper.dart
+++ lib/utils/file_helper.dart
@@ -2,6 +2,7 @@ import 'package:path/path.dart' as p;
 
 class SfFileHelper{
   static bool isUrl(String path) => path.startsWith(RegExp(r'http[s]?://'));
+  static bool isHttpsUrl(String url) => url.startsWith('https');
   static String getFileName(String path) => p.basename(path);
   static String getFileNameWithoutExt(String path) => p.basenameWithoutExtension(path);
   static String getFileExt(String path) => p.extension(path);
