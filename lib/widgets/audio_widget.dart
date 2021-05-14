0051bfbe1b001ae9af54c85b68affa8efea8a4a0
gohopo <gohopo@live.com>
2021-05-14 22:20:05 +0800

    aacADTS

--- lib/widgets/audio_widget.dart
+++ lib/widgets/audio_widget.dart
@@ -51,7 +51,7 @@ class SfAudioRecorderConsumerVM extends SfViewState{
     if(!status.isGranted) status = await Permission.microphone.request();
     if(!status.isGranted) throw '没有录音权限!';
     buildOverLay();
-    await _soundRecorder.startRecorder(toFile:'${SfLocatorManager.storageManager.voiceDirectory}/record.aac');
+    await _soundRecorder.startRecorder(codec:Codec.aacADTS,toFile:'${SfLocatorManager.storageManager.voiceDirectory}/record.aac');
     widget.onStartRecord?.call();
   }
   Future stop() async {
