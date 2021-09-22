19c72345066c75be89a55e478c5dd42242d49e18
gohopo <gohopo@live.com>
2021-09-22 14:26:26 +0800

    应付分身下，微信登录取消后多次，最后一次多次进入回调

--- lib/utils/wechat_helper.dart
+++ lib/utils/wechat_helper.dart
@@ -3,11 +3,12 @@ import 'dart:async';
 import 'package:fluwx/fluwx.dart';
 
 class WechatHelper{
+  static StreamSubscription subscription;
   static Future init(String appId) => registerWxApi(appId:appId,doOnIOS:false);
   static Future<WeChatAuthResponse> sendAuth() async {
     await sendWeChatAuth(scope:'snsapi_userinfo',state:'wechat_sdk_demo_test');
     var completer = Completer<WeChatAuthResponse>();
-    StreamSubscription subscription;
+    subscription?.cancel();
     subscription = weChatResponseEventHandler.listen((resp){
       subscription?.cancel();
       completer.complete(resp as WeChatAuthResponse);
