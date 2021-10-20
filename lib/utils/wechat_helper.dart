48084abb75e8ed7084cb03e1a76553bce3ad2a55
gohopo <gohopo@live.com>
2021-10-20 14:55:16 +0800

    universalLink

--- lib/utils/wechat_helper.dart
+++ lib/utils/wechat_helper.dart
@@ -4,7 +4,7 @@ import 'package:fluwx/fluwx.dart';
 
 class WechatHelper{
   static StreamSubscription subscription;
-  static Future init(String appId) => registerWxApi(appId:appId,doOnIOS:false);
+  static Future init(String appId,String universalLink) => registerWxApi(appId:appId,doOnIOS:false,universalLink:universalLink);
   static Future<WeChatAuthResponse> sendAuth() async {
     await sendWeChatAuth(scope:'snsapi_userinfo',state:'wechat_sdk_demo_test');
     var completer = Completer<WeChatAuthResponse>();
