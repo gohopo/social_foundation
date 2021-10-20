6f5fa703d9e83637927d3ce93919cc567667c849
gohopo <gohopo@live.com>
2021-10-20 15:07:19 +0800

    isWeChatInstalled

--- lib/utils/wechat_helper.dart
+++ lib/utils/wechat_helper.dart
@@ -4,6 +4,7 @@ import 'package:fluwx/fluwx.dart';
 
 class WechatHelper{
   static StreamSubscription subscription;
+  static Future<bool> get isInstalled => isWeChatInstalled;
   static Future init(String appId,String universalLink) => registerWxApi(appId:appId,doOnIOS:false,universalLink:universalLink);
   static Future<WeChatAuthResponse> sendAuth() async {
     await sendWeChatAuth(scope:'snsapi_userinfo',state:'wechat_sdk_demo_test');
