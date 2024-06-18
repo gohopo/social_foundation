9e29f1b0d65dede2b92e2f37d7e4d300f610e9e8
gohopo <gohopo@live.com>
2024-06-18 14:59:33 +0800

    SfSliverStickyAppBarDelegate

--- lib/widgets/sliver.dart
+++ lib/widgets/sliver.dart
@@ -45,102 +45,51 @@ class SfSliverStickyDelegateBuilder{
 
 class SfSliverStickyAppBarDelegate extends SliverPersistentHeaderDelegate{
   SfSliverStickyAppBarDelegate({
-    this.collapsedHeight = 32,
-    required this.expandedHeight,
-    this.paddingTop = 0,
-    this.background,
-    this.titleBuilder,
-    this.title,
-    this.goBackBuilder,
-    this.actionBuilder,
     this.actions = const [],
-    this.iconSize = 20
+    this.actionsBuilder,
+    this.background,
+    this.barBackgroundColor = Colors.white,
+    this.collapsedBrightness = Brightness.dark,
+    this.collapsedHeight = 32,
+    this.collapsedIconColor = Colors.white,
+    this.collapsedTextColor = Colors.transparent,
+    this.expandedBrightness = Brightness.light,
+    required this.expandedHeight,
+    this.expandedColor = Colors.black,
+    this.goBackBuilder,
+    this.iconSize = 20,
+    this.shrinkOffsetThreshold = 50,
+    this.paddingTop = 0,
+    this.title,
+    this.titleBuilder,
   });
-  final double collapsedHeight;
-  final double expandedHeight;
-  final double paddingTop;
-  final Widget? background;
-  final Widget Function(BuildContext context,SfSliverStickyAppBarDelegate delegate)? titleBuilder;
-  final String? title;
-  final Widget Function(BuildContext context,SfSliverStickyAppBarDelegate delegate)? goBackBuilder;
-  final List<Widget> Function(BuildContext context,SfSliverStickyAppBarDelegate delegate)? actionBuilder;
   final List<Widget> actions;
+  final List<Widget> Function(SfSliverStickyAppBarDelegate delegate,BuildContext context,double shrinkOffset,bool overlapsContent)? actionsBuilder;
+  final Widget? background;
+  final Color barBackgroundColor;
+  final Brightness collapsedBrightness;
+  final double collapsedHeight;
+  final Color collapsedIconColor;
+  final Color collapsedTextColor;
+  final Brightness expandedBrightness;
+  final Color expandedColor;
+  final double expandedHeight;
+  final Widget Function(SfSliverStickyAppBarDelegate delegate,BuildContext context,double shrinkOffset,bool overlapsContent)? goBackBuilder;
   final double iconSize;
-  late double shrinkOffset;
-  late bool overlapsContent;
-  String statusBarMode = 'dark';
-  late Color textColor;
-  late Color iconColor;
-
-  void updateStatusBarBrightness(){
-    if(shrinkOffset<=collapsedHeight && this.statusBarMode=='dark') {
-      this.statusBarMode = 'light';
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.light,
-        statusBarIconBrightness: Brightness.light,
-      ));
-    }
-    else if(shrinkOffset>collapsedHeight && this.statusBarMode=='light') {
-      this.statusBarMode = 'dark';
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.dark,
-        statusBarIconBrightness: Brightness.dark,
-      ));
-    }
-  }
-  Color makeBarBackgroundColor(){
-    final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
-    return Color.fromARGB(alpha, 255, 255, 255);
-  }
-  void updateBarTextColor(){
-    if(shrinkOffset <= 50){
-      textColor = Colors.transparent;
-      iconColor = Colors.white;
-    }
-    else{
-      final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
-      textColor = iconColor = Color.fromARGB(alpha, 0, 0, 0);
-    }
-  }
-  Widget buildBar(BuildContext context) => IconTheme(
-    data: IconThemeData(color:iconColor,size:iconSize),
-    child: Row(
-      children: [
-        if(Navigator.canPop(context)) Padding(
-          padding: EdgeInsets.only(left:15),
-          child: buildGoBackButton(context),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal:10),
-            child: title!=null ? buildTitle(context) : null,
-          )
-        ),
-        Padding(
-        padding: EdgeInsets.only(right:15),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: buildActionList(context),
-          ),
-        )
-      ],
-    )
-  );
-  Widget buildGoBackButton(BuildContext context) => GestureDetector(
-    onTap: () => Navigator.pop(context),
-    child: goBackBuilder?.call(context,this) ?? Icon(Icons.arrow_back_ios,size:iconSize),
-  );
-  Widget buildTitle(BuildContext context) => titleBuilder!=null ? titleBuilder!(context,this) : Text(title!,style:TextStyle(fontSize:iconSize,fontWeight:FontWeight.w500,color:textColor));
-  List<Widget> buildActionList(BuildContext context) => actionBuilder!=null ? actionBuilder!(context,this) : actions;
+  final double shrinkOffsetThreshold;
+  final double paddingTop;
+  final String? title;
+  final Widget Function(SfSliverStickyAppBarDelegate delegate,BuildContext context,double shrinkOffset,bool overlapsContent)? titleBuilder;
+  Brightness? statusBarBrightness;
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent){
-    this.shrinkOffset = shrinkOffset;
-    this.overlapsContent = overlapsContent;
-    this.updateStatusBarBrightness();
-    this.updateBarTextColor();
+  double get maxExtent => expandedHeight;
+  @override
+  double get minExtent => collapsedHeight + paddingTop;
+  @override
+  Widget build(BuildContext context,double shrinkOffset,bool overlapsContent){
+    this.updateStatusBarBrightness(shrinkOffset);
     return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: expandedHeight,
+      width:MediaQuery.of(context).size.width,height:expandedHeight,
       child: Stack(
         fit: StackFit.expand,
         children: [
@@ -149,10 +98,10 @@ class SfSliverStickyAppBarDelegate extends SliverPersistentHeaderDelegate{
             left:0,top:0,right:0,
             child: Container(
               padding: EdgeInsets.only(top:paddingTop),
-              color: makeBarBackgroundColor(),
+              color: getBarBackgroundColor(shrinkOffset),
               child: SizedBox(
                 height: collapsedHeight,
-                child: buildBar(context)
+                child: buildBar(context,shrinkOffset,overlapsContent)
               )
             )
           )
@@ -161,9 +110,49 @@ class SfSliverStickyAppBarDelegate extends SliverPersistentHeaderDelegate{
     );
   }
   @override
-  double get maxExtent => expandedHeight;
-  @override
-  double get minExtent => collapsedHeight + paddingTop;
-  @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) => true;
+  Widget buildBar(BuildContext context,double shrinkOffset,bool overlapsContent) => IconTheme(
+    data: IconThemeData(color:getIconColor(shrinkOffset),size:iconSize),
+    child: Row(
+      children: [
+        if(Navigator.canPop(context)) Padding(
+          padding: EdgeInsets.only(left:15),
+          child: buildGoBackButton(context,shrinkOffset,overlapsContent),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal:10),
+            child: buildTitle(context,shrinkOffset,overlapsContent),
+          )
+        ),
+        Padding(
+          padding: EdgeInsets.only(right:15),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: buildActions(context,shrinkOffset,overlapsContent),
+          ),
+        )
+      ],
+    )
+  );
+  Widget buildGoBackButton(BuildContext context,double shrinkOffset,bool overlapsContent) => GestureDetector(
+    onTap: () => Navigator.pop(context),
+    child: goBackBuilder?.call(this,context,shrinkOffset,overlapsContent) ?? Icon(Icons.arrow_back_ios,size:iconSize),
+  );
+  Widget buildTitle(BuildContext context,double shrinkOffset,bool overlapsContent) => titleBuilder?.call(this,context,shrinkOffset,overlapsContent) ?? Text(title??'',style:TextStyle(fontSize:iconSize,fontWeight:FontWeight.w500,color:getTextColor(shrinkOffset)));
+  List<Widget> buildActions(BuildContext context,double shrinkOffset,bool overlapsContent) => actionsBuilder?.call(this,context,shrinkOffset,overlapsContent) ?? actions;
+  double getOpacity(double shrinkOffset) => shrinkOffset / (this.maxExtent - this.minExtent);
+  Color getBarBackgroundColor(double shrinkOffset) => barBackgroundColor.withOpacity(getOpacity(shrinkOffset));
+  Color getIconColor(double shrinkOffset) => shrinkOffset<=shrinkOffsetThreshold ? collapsedIconColor : expandedColor.withOpacity(getOpacity(shrinkOffset));
+  Color getTextColor(double shrinkOffset) => shrinkOffset<=shrinkOffsetThreshold ? collapsedTextColor : expandedColor.withOpacity(getOpacity(shrinkOffset));
+  void updateStatusBarBrightness(double shrinkOffset){
+    if(shrinkOffset<=collapsedHeight && statusBarBrightness!=collapsedBrightness) {
+      statusBarBrightness = collapsedBrightness;
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarBrightness:collapsedBrightness,statusBarIconBrightness:collapsedBrightness));
+    }
+    else if(shrinkOffset>collapsedHeight && statusBarBrightness!=expandedBrightness) {
+      statusBarBrightness = expandedBrightness;
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarBrightness:expandedBrightness,statusBarIconBrightness:expandedBrightness));
+    }
+  }
 }
