397a3e63fbd4d95f3ed63fb1af852c1a09ef73f4
gohopo <gohopo@live.com>
2024-12-18 10:24:20 +0800

    fix: 解决Namespace问题
    Namespace not specified. Specify a namespace in the module's build file

--- lib/utils/wechat_helper.dart
+++ lib/utils/wechat_helper.dart
@@ -3,14 +3,15 @@ import 'dart:async';
 import 'package:fluwx/fluwx.dart';
 
 class WechatHelper{
-  static StreamSubscription? subscription;
-  static Future<bool> get isInstalled => isWeChatInstalled;
-  static Future init(String appId,String universalLink) => registerWxApi(appId:appId,doOnIOS:true,universalLink:universalLink);
+  static final _fluwx = Fluwx();
+  static FluwxCancelable? subscription;
+  static Future<bool> get isInstalled => _fluwx.isWeChatInstalled;
+  static Future init(String appId,String universalLink) => _fluwx.registerApi(appId:appId,doOnIOS:true,universalLink:universalLink);
   static Future<WeChatAuthResponse> sendAuth() async {
-    await sendWeChatAuth(scope:'snsapi_userinfo',state:'wechat_sdk_demo_test');
+    await _fluwx.authBy(which:NormalAuth(scope:'snsapi_userinfo',state:'wechat_sdk_demo_test'));
     var completer = Completer<WeChatAuthResponse>();
     subscription?.cancel();
-    subscription = weChatResponseEventHandler.listen((resp){
+    subscription = _fluwx.addSubscriber((resp){
       subscription?.cancel();
       completer.complete(resp as WeChatAuthResponse);
     });
