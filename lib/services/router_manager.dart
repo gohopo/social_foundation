f5f538376fa3270029ac6838e68109cb266e2588
gohopo <gohopo@live.com>
2025-03-11 09:54:36 +0800

    feat: SfRouterManager增加常用跳转方法

--- lib/services/router_manager.dart
+++ lib/services/router_manager.dart
@@ -1,6 +1,7 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:social_foundation/pages/photo_viewer.dart';
+import 'package:social_foundation/services/locator_manager.dart';
 import 'package:social_foundation/utils/aliyun_helper.dart';
 import 'package:social_foundation/widgets/cached_image_provider.dart';
 import 'package:social_foundation/widgets/page_route.dart';
@@ -10,6 +11,14 @@ class SfRouteName {
 }
 
 class SfRouterManager extends NavigatorObserver{
+  static NavigatorState getNavigator({NavigatorState? navigator,BuildContext? context}) => (navigator ?? (context!=null ? Navigator.of(context) : SfLocatorManager.routerManager.navigator))!;
+  static Future<T?> go<T extends Object?>(Widget page,{NavigatorState? navigator,BuildContext? context}) => route(SfMaterialRoute(page),navigator:navigator,context:context);
+  static Future<T?> goEmpty<T extends Object?>(Widget page,{NavigatorState? navigator,BuildContext? context}) => routeEmpty(SfMaterialRoute(page),navigator:navigator,context:context);
+  static Future<T?> goReplacement<T extends Object?>(Widget page,{NavigatorState? navigator,BuildContext? context}) => routeReplacement(SfMaterialRoute(page),navigator:navigator,context:context);
+  static Future<T?> route<T extends Object?>(Route<T> route,{NavigatorState? navigator,BuildContext? context}) => getNavigator(navigator:navigator,context:context).push(route);
+  static Future<T?> routeEmpty<T extends Object?>(Route<T> route,{NavigatorState? navigator,BuildContext? context}) => getNavigator(navigator:navigator,context:context).pushAndRemoveUntil(route,(_)=>false);
+  static Future<T?> routeReplacement<T extends Object?>(Route<T> route,{NavigatorState? navigator,BuildContext? context}) => getNavigator(navigator:navigator,context:context).pushReplacement(route);
+  static void pop<T extends Object?>({T? result,NavigatorState? navigator,BuildContext? context}) => getNavigator(navigator:navigator,context:context).pop(result);
   Future<T?> showPhotoViewer<T extends Object?>({required List<ImageProvider> images,int? index,String? heroPrefix,PageController? controller,bool? canSave,SfLoadStateChanged? loadStateChanged,List<Widget> Function(int index)? childrenBuilder}) async => navigator?.pushNamed<T>(SfRouteName.photo_viewer,arguments:{
     'images':images,'index':index,'heroPrefix':heroPrefix,'controller':controller,
     'canSave':canSave,'loadStateChanged':loadStateChanged,'childrenBuilder':childrenBuilder
