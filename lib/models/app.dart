416419d95cbfc35a6afde337be3fd27f5612e702
gohopo <gohopo@live.com>
2026-07-29 15:12:08 +0800

    isTablet

--- lib/models/app.dart
+++ lib/models/app.dart
@@ -1,26 +1,32 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:ui' as ui;
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:social_foundation/utils/file_helper.dart';
 
 class SfApp{
-  static bool get isHmOS => !Platform.isAndroid && !Platform.isIOS;
   static const ColorFilter greyColorFilter = ColorFilter.matrix(<double>[
     0.2126, 0.7152, 0.0722, 0, 0,
     0.2126, 0.7152, 0.0722, 0, 0,
     0.2126, 0.7152, 0.0722, 0, 0,
     0,      0,      0,      1, 0,
   ]);
-  static Future<String> prepareSound(String url) async {
-    if(!SfFileHelper.isUrl(url)){
-      var dir = SfFileHelper.getDirname(url);
-      var audioCache = AudioCache(prefix:'$dir/');
-      var uri = await audioCache.load(SfFileHelper.getFileName(url));
-      url = uri.toString();
-    }
-    return url;
+  static bool get isHmOS => !Platform.isAndroid && !Platform.isIOS;
+  static bool isTablet(){
+    // 获取当前首个视图的物理尺寸和像素比
+    final view = ui.PlatformDispatcher.instance.views.first;
+    final double pixelRatio = view.devicePixelRatio;
+    final ui.Size physicalSize = view.physicalSize;
+    
+    // 计算逻辑像素尺寸
+    final double logicalWidth = physicalSize.width / pixelRatio;
+    final double logicalHeight = physicalSize.height / pixelRatio;
+    
+    // 取短边判断
+    final double shortestSide = logicalWidth < logicalHeight ? logicalWidth : logicalHeight;
+    return shortestSide >= 600;
   }
   static Future<AudioPlayer> playSound(String url,{double volume=1.0,bool loop=false,ValueChanged<AudioPlayer>? onStopped}) async {
     url = await prepareSound(url);
@@ -36,5 +42,14 @@ class SfApp{
     if(loop) player.setReleaseMode(ReleaseMode.loop);
     player.play(UrlSource(url));
     return player;
+  }
+  static Future<String> prepareSound(String url) async {
+    if(!SfFileHelper.isUrl(url)){
+      var dir = SfFileHelper.getDirname(url);
+      var audioCache = AudioCache(prefix:'$dir/');
+      var uri = await audioCache.load(SfFileHelper.getFileName(url));
+      url = uri.toString();
+    }
+    return url;
   }
 }
